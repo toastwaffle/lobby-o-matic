ea61057a3d038f08cb411394c2c899b5e9577fad
Samuel Littley <samuel.littley@toastwaffle.com>
2012-08-08 10:33:54 +0100

    jQuery Mobile startup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title>Lobby-O-Matic</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="./resources/jquery.mobile-1.1.1.css" />
+	<link rel="stylesheet" href="./resources/jquery.mobile.structure-1.1.1.css" />
+	<link rel="stylesheet" href="./resources/jquery.mobile.theme-1.1.1.css" />
+	<script src="./resources/jquery-1.7.1.min.js"></script>
+	<script src="./resources/jquery.mobile-1.1.1.min.js"></script>
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>Lobby-O-Matic</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p>Hello world</p>		
+	</div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
